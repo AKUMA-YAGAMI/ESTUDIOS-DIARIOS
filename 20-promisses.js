5cf57ca8e137eaa6a6e6b9d5c773685c4d30230d
Enrique <arq.megreyes@gmail.com>
2025-07-16 12:38:26 -0600

    dia nuevo

--- 20-promisses.js
+++ 20-promisses.js
@@ -1,3 +1,5 @@
+/*
+
 const { resolve } = require("path");
 
 const miNumero = new Promise((resolve, reject) =>{
@@ -42,3 +44,93 @@ if(nUsuario.length > 3){
   .catch((rjct) => {
 console.log(rjct)
   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+const numPositive = new Promise((resolve, reject) => {
+  const nPositive = 1;
+  if (nPositive > 0) {
+    resolve('Número positivo');        // mensaje base
+  } else {
+    reject('Número negativo');
+  }
+});
+
+numPositive
+  .then(msg => msg + ' Validado')   // agrega texto
+  .then(msg => console.log(msg))       // imprime resultado final
+  .catch(err => console.log(err));     // maneja error
+
+
+
+
+
+*/
+
+
+
+
+
+/*
+
+
+
+
+
+
+const verificarEdad = new Promise((resolve, reject) =>{
+const edad = 17;
+
+if(edad >= 18){
+    resolve(`edad valida de ${edad}`)
+}else{
+    reject('Acceso denegado')
+}
+
+
+
+});
+
+verificarEdad
+.then(revisionEdad => revisionEdad + ' ......accediendo al sistema' )
+.then(revisionEdad =>console.log (revisionEdad)) 
+
+ 
+.catch(err => console.log('error', err));
+
+*/
+
+
+
+
+
+
+
+
+const newUser = new Promise((resolve, reject) =>{
+const usuario = 'Akuma';
+const password = '1234';
+
+if (usuario.length >= 3 && password >= 3){
+    resolve(usuario)
+}else{
+    reject('el nombre de usuario debe ser de mas de 3 letras al igual la contraseña')
+}
+
+
+});
+
+newUser
+.then(user => user + ' estamos procesando tu informacion ........')
+.then(user =>{console.log(`tu nombre de usuario es ${user}  fuiste registrado exitosamente`)})
+.catch(err => console.log(err));
